9f645694770231a57e99c45587b53bba8ea29073
vcephirins <vcephirins@gmail.com>
2017-10-27 16:15:51 +0200

    try catch management + root rest url

--- src/main/java/fr/cnes/cubeExplorer/resources/GeoJsonResponse.java
+++ src/main/java/fr/cnes/cubeExplorer/resources/GeoJsonResponse.java
@@ -5,7 +5,6 @@
 package fr.cnes.cubeExplorer.resources;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -13,30 +12,25 @@ import org.json.JSONObject;
  *
  */
 public class GeoJsonResponse {
-	
+
 	private JSONObject geoJson = new JSONObject();
 
 	/**
 	 * 
 	 */
 	public GeoJsonResponse(int coordx, int coordy, JSONObject properties) {
-        JSONObject geoJsonFeature = new JSONObject();
-        JSONObject geoJsonGeometry = new JSONObject();
-        JSONArray geoJsonCoordinates = new JSONArray();
-        
-        geoJsonCoordinates.put(coordx);
-        geoJsonCoordinates.put(coordy);
-        try {
-			geoJsonGeometry.put("type", "Point");
-			geoJsonGeometry.put("coordinates", geoJsonCoordinates);
-			geoJsonFeature.put("type", "Feature");
-			geoJsonFeature.put("geometry", geoJsonGeometry);
-			geoJsonFeature.put("properties", properties);
-			geoJson.put("feature", geoJsonFeature);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		JSONObject geoJsonFeature = new JSONObject();
+		JSONObject geoJsonGeometry = new JSONObject();
+		JSONArray geoJsonCoordinates = new JSONArray();
+
+		geoJsonCoordinates.put(coordx);
+		geoJsonCoordinates.put(coordy);
+		geoJsonGeometry.put("type", "Point");
+		geoJsonGeometry.put("coordinates", geoJsonCoordinates);
+		geoJsonFeature.put("type", "Feature");
+		geoJsonFeature.put("geometry", geoJsonGeometry);
+		geoJsonFeature.put("properties", properties);
+		geoJson.put("feature", geoJsonFeature);
 	}
 
 	/**
